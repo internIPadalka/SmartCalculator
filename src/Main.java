9204ed3cb12658b132b70b8b70263595d932495f
IvapPadalka <ipadalka@griddynamics.com>
2022-03-26 10:24:08 +0200

    Implemented stage 5

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,114 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Main {
+interface SmartCalculator {
+    void addition(String [] array);
+}
 
-    public static void main(String[] args) {
+class Addition implements SmartCalculator{
+    private Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void addition(String [] array) {
+        int sum = 0;
+        boolean isAurevoir = false;
+
         do {
-            Scanner scanner = new Scanner(System.in);
-            String out = scanner.nextLine();
-            if (out.equals("/exit")) {
-                System.out.println("Bye!");
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("/exit")){
+                GeneralInfos.bye();
                 break;
-            }else if (out.equals("/help")) {
-                System.out.println("The program calculates the sum of numbers");
+            } else if (userInput.equals("/help")){
+                GeneralInfos.help();
                 continue;
-            }else {
-                List<String> list=Arrays.asList(out.split(" "));
-                if (list.isEmpty()) {
-                    continue;
-                }else {
-                    if (list.get(0)==" " || list.get(0)=="") {
-                        continue;
-                    }else if(list.size()==1){
-                        System.out.println(Integer.parseInt(list.get(0)));
-                    }else {
-                        int sum = 0;
-                        for (String counter:list) {
-                            sum+=Integer.parseInt(counter);
-                        }
-                        System.out.println(sum);
+            } else if (userInput.equals("")) {
+                continue;
+            } else if (userInput.substring(0,1).equals("/")){
+                System.out.println("Unknown command");
+                continue;
+            }
+
+            String userInputcleaner = GeneralInfos.regexCleaner(userInput);
+            try {
+                array = userInputcleaner.split(" ");
+                sum = Integer.parseInt(array[0]);
+                for (int i = 1; i < array.length; i+=2){
+                    if(array[i].equals("+")){
+                        sum += Integer.parseInt(array[i+1]);
+                    }else if (array[i].equals("-")){
+                        sum -= Integer.parseInt(array[i+1]);
                     }
                 }
+                System.out.println(sum);
+            } catch (NumberFormatException ignored) {
+                System.out.println("Invalid expression");
+                continue;
+            } catch (ArrayIndexOutOfBoundsException exception){
+                System.out.println("Invalid expression");
+                continue;
             }
-        }while (true);
+        } while (!isAurevoir);
+    }
+}
+
+class SmartCalculatorContext {
+    private SmartCalculator smartCalculator;
+    void setCalculationMethod(SmartCalculator smartCalculator){
+        this.smartCalculator = smartCalculator;
+    }
+
+    void addition(String [] array) {
+        this.smartCalculator.addition(array);
+    }
+}
+
+class GeneralInfos {
+    static void bye(){
+        System.out.println("Bye!");
+    }
+
+    static void help(){
+        System.out.println("The program support the:" +
+                "\nAddition + and Subtraction - operators:" +
+                "\nConsider that the even number of minuses gives a plus," +
+                "\nand the odd number of minuses gives a minus! Look at it this way:" +
+                "\n2 -- 2 equals 2 - (-2) equals 2 + 2.");
+    }
+
+    static String regexCleaner(String userInput) {
+        Pattern extraSpace = Pattern.compile("\\s+");
+        Matcher matchSpace = extraSpace.matcher(userInput);
+        String spaceCleaner = matchSpace.replaceAll(" ");
+
+        Pattern patternPLUS = Pattern.compile("\\+{2,}");
+        Matcher matcherPLUS = patternPLUS.matcher(spaceCleaner);
+        String mergePLUS = matcherPLUS.replaceAll("+");
+
+        Pattern patternMINUS = Pattern.compile("-{3}");
+        Matcher matcherMINUS = patternMINUS.matcher(mergePLUS);
+        String mergeMINUS = matcherMINUS.replaceAll("-");
+
+        Pattern patternDOUBLEMINUS = Pattern.compile("-{2}");
+        Matcher matcherDOUBLEMINUS = patternDOUBLEMINUS.matcher(mergeMINUS);
+        String mergeDOUBLEMINUS = matcherDOUBLEMINUS.replaceAll("+");
+
+        Pattern patternStartByMINUS = Pattern.compile("\\b-.*?");
+        Matcher matcherStartByMINUS = patternStartByMINUS.matcher(mergeDOUBLEMINUS);
+        String refactoringZERO = matcherStartByMINUS.replaceFirst("0 - ");
+
+        Pattern finalCheck = Pattern.compile("(-\\+|\\+-)");
+        Matcher finalMatch = finalCheck.matcher(refactoringZERO);
+        String result = finalMatch.replaceAll("-");
+        return result;
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        SmartCalculatorContext calculMethod = new SmartCalculatorContext();
+        calculMethod.setCalculationMethod(new Addition());
+        calculMethod.addition(null);
     }
 }
